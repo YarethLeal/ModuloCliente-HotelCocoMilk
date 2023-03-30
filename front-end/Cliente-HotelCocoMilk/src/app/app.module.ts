eaa9510d21eca140617b005fbe7ef4516d822b0c
Kendall2106 <kenbrojim@gmail.com>
2023-03-30 01:51:34 -0600

    Construccion de la pagina de Home en el front de cliente

--- front-end/Cliente-HotelCocoMilk/src/app/app.module.ts
+++ front-end/Cliente-HotelCocoMilk/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './core/componentes/menu/menu.component';
+import { FooterComponent } from './core/componentes/footer/footer.component';
+import { HeaderComponent } from './core/componentes/header/header.component';
+import { HomeComponent } from './paginas/home/home.component';
+import { PublicityComponent } from './core/componentes/publicity/publicity.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    FooterComponent,
+    HeaderComponent,
+    HomeComponent,
+    PublicityComponent
   ],
   imports: [
     BrowserModule,
---
+++ front-end/Cliente-HotelCocoMilk/src/app/core/componentes/publicity/publicity.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-publicity',
+  templateUrl: './publicity.component.html',
+  styleUrls: ['./publicity.component.css']
+})
+export class PublicityComponent {
+
+}
+
+
