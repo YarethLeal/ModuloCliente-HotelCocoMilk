0208504abfb6f5428bafb58d14ec5c9a31a83cd0
AracelySC <84749072+AracelySC@users.noreply.github.com>
2023-03-29 21:09:06 -0600

    Data Clases

--- back-end/AccesoADatos/Data/DataCliente.cs
+++ back-end/AccesoADatos/Data/DataCliente.cs
@@ -1,5 +1,4 @@
-﻿
-using AccesoADatos.Context;
+﻿using AccesoADatos.Context;
 using Microsoft.EntityFrameworkCore;
 using Entidades.Entities;
 using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
