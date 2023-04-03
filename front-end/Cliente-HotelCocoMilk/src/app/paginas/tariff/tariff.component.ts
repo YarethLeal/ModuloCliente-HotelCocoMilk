41d8160869a3568845163afc32c2bd51029be1b5
Kendall2106 <kenbrojim@gmail.com>
2023-04-02 23:41:19 -0600

    Tarifas, la info se muestra bien

--- front-end/Cliente-HotelCocoMilk/src/app/paginas/tariff/tariff.component.ts
+++ front-end/Cliente-HotelCocoMilk/src/app/paginas/tariff/tariff.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TipoHabitacion } from 'src/app/core/modelos/tipoHabitacion.model';
+import { TipoHabitacionService } from 'src/app/core/servicios/tipoHabitacion.service';
+
+//let dataTipoHabitacion: TipoHabitacion[];
 
 @Component({
   selector: 'app-tariff',
   templateUrl: './tariff.component.html',
   styleUrls: ['./tariff.component.css']
 })
-export class TariffComponent {
+export class TariffComponent implements OnInit{
+
+  public id_habitacion: number;
+  public imagen: string;
+  public tipo: string;
+  public informacion: string;
+  public  tarifa: number;
+
+  constructor (private tipoHabitacionService: TipoHabitacionService) {
+    this.id_habitacion=0;
+    this.imagen='';
+    this.tipo='';
+    this.informacion='';
+    this.tarifa=0;
+   // dataTipoHabitacion=[];
+  }
+  
+
+  ngOnInit(): void {
+    this.listarTipoHabitacion();
+   // this.obtenerTipoDeCambioXML();
+  }
+
+  dataTipoHabitacion: any = [];
+
+  listarTipoHabitacion() {
+    this.tipoHabitacionService.listarTipoHabitacion().subscribe((data: TipoHabitacion[]) => {
+      this.dataTipoHabitacion = data;
+     /* this.informacion=data[0].informacion;
+      this.imagen=data[0].imagen;*/
+    });
+}
+
+obtenerTipoDeCambioXML(){
+  this.tipoHabitacionService.obtenerTipoDeCambioXML().subscribe((respuesta: any) => {
+    //this.dialogoNotificacion(respuesta);
+    console.log(respuesta);
+  });
+}
+
 
 }
+
+
+
+
