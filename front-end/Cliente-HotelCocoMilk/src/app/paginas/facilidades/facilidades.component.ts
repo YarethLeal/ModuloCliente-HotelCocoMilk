da2e1d41ad525fc624884d63491d9e819eefc150
AracelySC <84749072+AracelySC@users.noreply.github.com>
2023-04-03 22:27:12 -0600

    Avance facilidades

--- front-end/Cliente-HotelCocoMilk/src/app/paginas/facilidades/facilidades.component.ts
+++ front-end/Cliente-HotelCocoMilk/src/app/paginas/facilidades/facilidades.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
+import { Pagina } from 'src/app/core/modelos/pagina.model';
+import { PaginaService } from 'src/app/core/servicios/pagina.service';
 
 @Component({
   selector: 'app-facilidades',
   templateUrl: './facilidades.component.html',
   styleUrls: ['./facilidades.component.css']
 })
-export class FacilidadesComponent {
+export class FacilidadesComponent implements OnInit{
 
+ dataPaginas: Pagina[] =[];
+  constructor(private paginaService: PaginaService) {
+
+  }
+
+  ngOnInit(): void {
+    this.mostrarPagina("Facilidades");
+  }
+
+  mostrarPagina(buscar: string) {
+    this.paginaService.mostrarPagina({ tipoPagina: buscar }).subscribe((data: Pagina[]) => {
+      this.dataPaginas = data;
+
+    });
+}
 }
