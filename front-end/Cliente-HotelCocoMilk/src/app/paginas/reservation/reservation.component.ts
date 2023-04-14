3b89da4f6a12c179ceca0b1ca6d8f8c0c03299c0
Kendall2106 <kenbrojim@gmail.com>
2023-04-13 20:48:13 -0600

    cambios en reservacion

--- front-end/Cliente-HotelCocoMilk/src/app/paginas/reservation/reservation.component.ts
+++ front-end/Cliente-HotelCocoMilk/src/app/paginas/reservation/reservation.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TipoHabitacion } from 'src/app/core/modelos/tipoHabitacion.model';
+import { TipoHabitacionService } from 'src/app/core/servicios/tipoHabitacion.service';
 
 @Component({
   selector: 'app-reservation',
   templateUrl: './reservation.component.html',
   styleUrls: ['./reservation.component.css']
 })
-export class ReservationComponent {
+export class ReservationComponent implements OnInit{
+
+  public fechaInicio: Date;
+  public fechaFinal: Date;
+
+  constructor (private tipoHabitacionService: TipoHabitacionService) {
+    this.fechaInicio=new Date();
+    this.fechaFinal=new Date();
+    
+  }
+
+  ngOnInit(): void {
+    this.listarTipoHabitacion();
+    console.log(this.fechaInicio);
+  }
+
+  dataTipoHabitacion: any = [];
+
+  listarTipoHabitacion() {
+    this.tipoHabitacionService.listarTipoHabitacion().subscribe((data: TipoHabitacion[]) => {
+      this.dataTipoHabitacion = data;
+    });
+}
 
 }
